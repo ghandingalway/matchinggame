6fdbc9b1e95de133ce7fe960c9fa5cdd5db9bf37
Grainne Hand <ghandingalway@users.noreply.github.com>
2014-07-03 20:19:09 +0100

    Added sound to the cards

--- Game1.js
+++ Game1.js
@@ -1,10 +1,12 @@
 (function(){
+	$(document).ready(function()
+{
 	var animals = []
-		var elephant = {name:'elephant', source:'http://www.clker.com/cliparts/8/6/d/2/12161376851595770453lemmling_Cartoon_elephant.svg.hi.png', bborder:'#ffc0cb', fborder:'#ff0000'};
-		var tiger = {name:'tiger', source:'http://www.clipartlord.com/wp-content/uploads/2013/07/tiger4.png', bborder:'#add8e6', fborder:'#0000ff'};
-		var monkey = {name:'monkey', source:'http://sweetclipart.com/multisite/sweetclipart/files/monkey_with_banana.png', bborder:'#98fb98', fborder:'#008000'};
-		var snake = {name:'snake', source:'http://3.bp.blogspot.com/-YyVUBri5SJs/UiDFZAiHB7I/AAAAAAAAA-s/fbxppoYg-QE/s1600/cartoon-snake-clipart.jpg', bborder:'#ffffe0', fborder:'ffff00'};
-		var crocodile = {name:'crocodile', source:'http://addison.typepad.com/.a/6a00d8341c1a9053ef01a73dd31b6f970d-pi', bborder:'#dda0dd', fborder:'#800080'};
+		var elephant = {name:'elephant', source:'http://www.clker.com/cliparts/8/6/d/2/12161376851595770453lemmling_Cartoon_elephant.svg.hi.png', bborder:'#ffc0cb', fborder:'#ff0000', sound:$("#elephant1")[0]};
+		var tiger = {name:'tiger', source:'http://www.clipartlord.com/wp-content/uploads/2013/07/tiger4.png', bborder:'#add8e6', fborder:'#0000ff', sound:$("#tiger1")[0]};
+		var monkey = {name:'monkey', source:'http://sweetclipart.com/multisite/sweetclipart/files/monkey_with_banana.png', bborder:'#98fb98', fborder:'#008000', sound:$("#monkey1")[0]};
+		var snake = {name:'snake', source:'http://3.bp.blogspot.com/-YyVUBri5SJs/UiDFZAiHB7I/AAAAAAAAA-s/fbxppoYg-QE/s1600/cartoon-snake-clipart.jpg', bborder:'#ffff90', fborder:'#ffff00', sound:$("#snake1")[0]};
+		var crocodile = {name:'crocodile', source:'http://addison.typepad.com/.a/6a00d8341c1a9053ef01a73dd31b6f970d-pi', bborder:'#dda0dd', fborder:'#800080', sound:$("#crocodile1")[0]};
 	animals.push(elephant);animals.push(elephant); animals.push(tiger); animals.push(tiger); animals.push(monkey); animals.push(monkey); animals.push(snake); animals.push(snake); animals.push(crocodile); animals.push(crocodile);
 		var mixedanimals = [];
 	while(animals.length>0)
@@ -13,13 +15,20 @@
 		mixedanimals.push(animals[i]);
 		animals.splice(i,1);
 	}
-$(document).ready(function()
-{
+	$(".imag").each(function(i)
+	{
+		$(this).css("border-color", mixedanimals[i].bborder);
+	});
 	$(".imag").each(function(i)
 	{
 		$("img", this).click(function()
 		{
 			$(this).attr("src", mixedanimals[i].source);
+		});
+		$(this).click(function()
+		{
+			$(this).css("border-color", mixedanimals[i].fborder);
+			mixedanimals[i].sound.play();
 		});
 	});
 });
