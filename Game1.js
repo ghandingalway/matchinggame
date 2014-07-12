0b52bbf81c8303944f87bd3e2b0a506d43d60d8c
Grainne Hand <ghandingalway@users.noreply.github.com>
2014-07-12 21:51:27 +0100

    Made the functions more flewible
    To allow me to vary the animals array. Still can't get the call back to work

--- Game1.js
+++ Game1.js
@@ -1,57 +1,72 @@
 (function(){
 $(document).ready(function()
 {	
-	var elephant = {name:'elephant', sound:$("#elephant1")[0]};
+	var elephant = {name:'elephant', sound:$("#elephant1")[0], special:$('#Nellie1')[0]};
 	var tiger = {name:'tiger', sound:$("#tiger1")[0]};
 	var monkey = {name:'monkey', sound:$("#monkey1")[0]};
 	var snake = {name:'snake', sound:$("#snake1")[0]};
 	var crocodile = {name:'crocodile', sound:$("#crocodile1")[0]};
-	var animals = [elephant, elephant, tiger, tiger, monkey, monkey, snake, snake, crocodile, crocodile];
-	var cow = {name:'cow', sound:$('#cow1')[0]};
-	var horse = {name:'horse', sound:$('#horse1')[0]};
-	var pig = {name:'pig', sound:$('#pig1')[0]};
-	var chicken = {name:'chicken', sound:$('#chicken1')[0]};
-	var duck = {name:'duck', sound:$('#duck1')[0]};
+	var animals = [];
+	var cow = {name:'cow', sound:$("#cow1")[0]};
+	var horse = {name:'horse', sound:$("#horse1")[0]};
+	var pig = {name:'pig', sound:$("#pig1")[0]};
+	var chicken = {name:'chicken', sound:$("#chicken1")[0]};
+	var duck = {name:'duck', sound:$("#duck1")[0], special:$("#FiveDucks")[0]};
 	var counter = "";
 	var applause = $('#applause')[0];
-	var Nellie = $('#Nellie1')[0];
-	var FiveDucks = $('#FiveDucks')[0];
 	var mixedanimals = [];
 	var lastpress;
 	var matchingcounter =0;
-	while(animals.length>0)
+	var init = function(animal1, animal2, animal3, animal4, animal5)
 	{
-		var i = Math.floor(Math.random()*animals.length);
-		mixedanimals.push(animals[i]);
-		animals.splice(i,1);
+		counter = "";
+		mixedanimals = [];
+		matchingcounter = 0;
+		animals = [animal1, animal1, animal2, animal2, animal3, animal3, animal4, animal4, animal5, animal5];
+		while(animals.length>0)
+		{
+			var i = Math.floor(Math.random()*animals.length);
+			mixedanimals.push(animals[i]);
+			animals.splice(i,1);
+		}
+		for(var i=0; i<mixedanimals.length; i++)
+		{
+			mixedanimals[i].matched = false;
+		}
+		$(".imag").each(function(i)
+		{
+			$(this).toggleClass(mixedanimals[i].name, true);
+			$(this).toggleClass("facedown", true);
+		});
 	}
-	for(var i=0; i<mixedanimals.length; i++)
-	{
-		mixedanimals[i].matched = false;
-	}
+	init(cow, horse, pig, chicken, duck);
 	var fireworks = function()
 	{	
-		$(".imag").css("visibility", "hidden");
-		$(".imag.elephant").css("visibility", "visible");
-		$(".imag.elephant").each().toggleClass("fireworks");
-		$(".imag.elephant").first().animate({left:"15%"});
-		$(".imag.elephant").last().animate({left:"60%"});
-		var Nellie = $('#Nellie1')[0];
-	//	Nellie.play();
-		for(var k=0; k<30; k++)
+		var fireworking = function(spec, name)
 		{
-			$(".imag.elephant").animate({top:"5%"});
-			$(".imag.elephant").animate({top:"10%", left:"+=5%"});
-			$(".imag.elephant").animate({top:"15%", left:"-=5%"});
-			$(".imag.elephant").animate({top:"10%", left:"-=5%"});
-			$(".imag.elephant").animate({top:"10%", left:"+=5%"});
+		$(".imag").css("visibility", "hidden");
+		$(".imag."+name).css("visibility", "visible");
+		$(".imag."+name).first().toggleClass("fireworks1", true);
+		$(".imag."+name).last().toggleClass("fireworks2", true);
+		spec.play();
+		for(var k=0; k<30; k++)
+			{
+				$(".imag."+name).animate({top:"5%"});
+				$(".imag."+name).animate({top:"10%", left:"+=5%"});
+				$(".imag."+name).animate({top:"15%", left:"-=5%"});
+				$(".imag."+name).animate({top:"10%", left:"-=5%"});
+				$(".imag."+name).animate({top:"10%", left:"+=5%"});
+			}
 		}
-		
-	}
-	$(".imag").each(function(i)
-	{
-		$(this).toggleClass(mixedanimals[i].name, true)
-	});
+		for(var j=0; j<mixedanimals.length; j++)
+		{
+			if(mixedanimals[j].special)
+			{
+				fireworking(mixedanimals[j].special, mixedanimals[j].name);
+			}
+		}
+	init(elephant, tiger, monkey, snake, crocodile)}
+	
 	$(".imag").each(function(i)
 	{
 		$(this).mousedown(function()
@@ -65,15 +80,15 @@ $(document).ready(function()
 			mixedanimals[i].sound.play();
 			$(this).toggleClass('facedown', false);
 			lastpress = i;
-			counter = mixedanimals[i].name;
+			counter = mixedanimals[i];
 			}
-			else if((counter === mixedanimals[i].name)&&(lastpress!==i))
+			else if((counter.name === mixedanimals[i].name)&&(lastpress!==i))
 			{
 				mixedanimals[i].sound.play();
 				$(this).toggleClass('facedown', false);
-				if (counter === elephant.name)
+				if (mixedanimals[i].special)
 				{
-					Nellie.play();
+					mixedanimals[i].special.play();
 				}
 				else 
 				{
@@ -81,20 +96,20 @@ $(document).ready(function()
 				}
 				$(".imag").each(function(j)
 				{
-					if(mixedanimals[j].name === counter)
+					if(mixedanimals[j].name === counter.name)
 					{
 						mixedanimals[j].matched = true;
 						matchingcounter = matchingcounter +1;
 						if(mixedanimals.length===matchingcounter)
 						{
-						fireworks();
+						fireworks(init);
 						}
 					}
 				});	
 				lastpress = i;
 				counter = "";
 			}
-			else if((counter === mixedanimals[i].name)&&(lastpress===i))
+			else if((counter.name === mixedanimals[i].name)&&(lastpress===i))
 			{
 				$(this).toggleClass('facedown', true);
 				counter = "";
